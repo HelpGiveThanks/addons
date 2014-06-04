b3e49ae2993b6b31a445f0d3190996ff38a97b7c
David Matson <githubdavid@gmail.com>
2014-06-03 17:11:17 -0700

    Updated plugin remix notice

--- plugins/QnA/class.qna.plugin.php
+++ plugins/QnA/class.qna.plugin.php
@@ -9,15 +9,13 @@ $PluginInfo['QnA'] = array(
    'Name' => 'Q&A Big Button Remix',
    'Description' => "Users may designate a discussion as a Question and then officially accept one or more of the comments as the answer.  
 
-NOTE: David Matson made code changes altering this plugin from v1.2.1 as specified by peregrine in this very helpful post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button.  This remix is provided as a convenience saving you the time altering code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1 and provide corrections here: https://github.com/HelpGiveThanks/addons/tree/master/plugins/QnA
+NOTE: David Matson altered this plugin coping and pasting code changes found in the helpful forum post The Big Button option part A: http://vanillaforums.org/discussion/26902/some-adjustments-to-more-clearly-see-ask-a-question-in-q-a-1-2-1-new-discussion-button.  This remix is provided as a convenience saving you the time altering the original Q&A plugin code yourself to put a big 'Ask a Question' button on your Vanilla forum, and is provided to you under a GNU GPL2 license just like the original. Compare original and remixed versions of this plugin here: https://github.com/HelpGiveThanks/addons/compare/vanilla:2.1...2.1.
 
-To upgrade this plugin remix you will need to download the latest original (unremixed) version of the plugin, and modify it with peregrine's code changes yourself, or hire someone like peregrine to do it for you: http://vanillaforums.org/addon/734/q-a.",
+To upgrade this plugin remix you will need to download the latest original (unremixed) version of the plugin http://vanillaforums.org/addon/734/q-a, and modify it with code changes found in the above forum post yourself, or hire someone to do it for you. You can check at the Help Give Thanks SourceForge folder to see if there is an upgrade available, but no promises: http://sourceforge.net/projects/helpgivethanks/files/HGTtemplate/",
    'Version' => '1.2.1 Big Button Remix',
    'RequiredApplications' => array('Vanilla' => '2.0.18'),
    'MobileFriendly' => TRUE,
-   'Author' => 'Todd Burry (original), David Matson (remixer adding in code changes authored by peregrine)',
-   'AuthorEmail' => 'todd@vanillaforums.com',
-   'AuthorUrl' => 'http://www.vanillaforums.org/profile/todd'
+   'Author' => 'David Matson (remixer only adding in a very small number of code changes suggested by peregrine), Todd Burry (original author)'
 );
 
 /**
